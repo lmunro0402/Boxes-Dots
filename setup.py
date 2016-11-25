9e786ba9ef13c64947d59697516ec4fcdc341eda
Luke <lmunro@calpoly.edu>
2016-11-25 00:59:50 -0800

    cool

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Creat files for talk between NN and game
+
+def main():
+	with open('data', 'w') as d:
+		d.write("")
+
+	with open('move', 'w') as d:
+		d.write("")
+
+if __name__ == '__main__':
+	main()
